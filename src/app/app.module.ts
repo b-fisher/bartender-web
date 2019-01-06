908280e83524586b9197f792bfebcc56afdb17ea
Blake Fisher <blakefisher@gmail.com>
2019-01-06 22:12:17 +0000

    README update and temp inline CSS to be replaced

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatButtonModule, MatCheckboxModule } from '@angular/material';
+import { MatButtonModule, MatCheckboxModule, MatCardModule } from '@angular/material';
 import { AppComponent } from './app.component';
 import { DrinksComponent } from './drinks/drinks.component';
 
@@ -16,7 +16,8 @@ import { DrinksComponent } from './drinks/drinks.component';
     AppRoutingModule,
     BrowserAnimationsModule,
     MatButtonModule,
-    MatCheckboxModule
+    MatCheckboxModule,
+    MatCardModule
   ],
   providers: [],
   bootstrap: [AppComponent]
